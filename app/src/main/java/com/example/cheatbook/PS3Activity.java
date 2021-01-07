3361f8a4ff51081c5f49d63f80d93d97e72e20a7
Aditya Galande <adityagalande14@gmail.com>
2021-01-07 16:57:50 +0530

    Intent activity and image resource added

---
+++ app/src/main/java/com/example/cheatbook/PS3Activity.java
@@ -0,0 +1,14 @@
+package com.example.cheatbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PS3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_s3);
+    }
+}
