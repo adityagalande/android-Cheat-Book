c62f15eed0f14f8f75d49af013945f5e7694c896
Aditya Galande <adityagalande14@gmail.com>
2021-02-10 22:54:51 +0530

    about section created

--- app/src/main/java/com/example/cheatbook/About.java
+++ app/src/main/java/com/example/cheatbook/About.java
@@ -4,16 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class About extends AppCompatActivity {
 
+    private String version = "Version 1.0.0";
+    private String content = "Nurtured from the seed of a single great idea - to empower the Gaming Enthusiast.\n" +
+            "CheatBook is a pioneer in Gamer’s community.\n" +
+            "Founded by Aditya Galande,\n" +
+            "CheatBook came to life to support the Gaming enthusiast with Tips, Tricks, Hints and Cheat Codes comprehensive choices.\n" +
+            "CheatBook also offering Tips, Tricks and Hints with different platforms like PC, Playstation3, PlayStation4, Xbox etc.\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        try {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        } catch (NullPointerException ignored) {
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        getSupportActionBar().setTitle("About!!!");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = findViewById(R.id.about_content);
+        textView.setText(content);
+
+        TextView textViewVersion = findViewById(R.id.about_version);
+        textViewVersion.setText(version);
     }
 }
